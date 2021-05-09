af864d8c5cf0f042b64943dd0234ce6b09abe614
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-05-09 17:41:42 +0200

    Space clean

--- zeri_di_funzioni/stima_ordine.py
+++ zeri_di_funzioni/stima_ordine.py
@@ -5,4 +5,4 @@ Metodo per la stima dell'ordine
 import numpy as np
 def stima_ordine(xks,num_iterazioni):
     k = num_iterazioni - 3
-    return np.log(abs(xks[k+2]-xks[k+3])/abs(xks[k+1]-xks[k+2])) / np.log(abs(xks[k+1]-xks[k+2]) / abs(xks[k]-xks[k+1]))
+    return np.log(abs(xks[k+2]-xks[k+3])/abs(xks[k+1]-xks[k+2])) / np.log(abs(xks[k+1]-xks[k+2])/abs(xks[k]-xks[k+1]))
