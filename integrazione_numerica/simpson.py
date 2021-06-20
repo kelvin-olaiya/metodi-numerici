Correct node spacing calculation

integrazione_numerica/simpson.py
@@ -5,7 +5,7 @@ Metodo dei Simpson
 import numpy as np
 
 def simpson_composita(func, a, b, n):
-    h = (b-a)/2*n # ! focus here
+    h = (b-a)/(2*n) # ! focus here
     nodi = np.arange(a, b+h, h)
     f = func(nodi)
     return (f[0] + 2*np.sum(f[2:2*n:2]) + 4*np.sum(f[1:2*n:2]) + f[n])*h/3 # ! focus here