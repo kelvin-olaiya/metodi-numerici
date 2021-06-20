a231f4a857074d811eb230b1114545f05fabe65f
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-06-20 14:34:21 +0200

    Correct QRLS method

--- approssimazione_mimimi_quadrati/metodo_QR.py
+++ approssimazione_mimimi_quadrati/metodo_QR.py
@@ -4,9 +4,19 @@ Approssimazione con metodo QR
 """
 import numpy as np
 import scipy.linalg as spl
-from sistemi_lineari import solveU
+
+def Usolve(U, b):
+    m,n = U.shape
+    if m != n and not np.all(U.diag()):
+        print("Metodo non applicabile")
+        return []
+    x = np.zeros((n,1))
+    for k in reversed(range(n)):
+        sommatoria = np.dot(U[k,k+1:], x[k+1:])
+        x[k] = (b[k] - sommatoria) / U[k,k]
+    return x
 
 def metodoQR(x,y,n):
     Q,R = spl.qr(np.vander(x,n+1))
     ys = np.dot(Q.T,y)
-    return solveU(R[:n+1],ys[:n+1])
+    return Usolve(R[n+1:],ys[n+1:])
