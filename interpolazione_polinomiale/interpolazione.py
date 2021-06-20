22cd3fa2658a94af332a0d54ae43c4075b4eb2b1
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-06-20 14:24:48 +0200

    Correct interpolation method and minor refactoring

--- interpolazione_polinomiale/interpolazione.py
+++ interpolazione_polinomiale/interpolazione.py
@@ -10,19 +10,18 @@ import numpy as np
 # Restituisce i coefficienti del k-esimo polinomio
 # di Lagrange
 def polinomioLagrange(xnodi,k):
-    n = xnodi.size
     if k == 0:
-        xzeri = xnodi[1:n]
+        xzeri = xnodi[1:]
     else:
-        xzeri = np.append(xnodi[0:k],xnodi[k+1:n])
+        xzeri = np.append(xnodi[0:k],xnodi[k+1:])
     num = np.poly(xzeri) # np.poly restituisce i coeff. del polinomi che ha per zeri xzeri
     den = np.polyal(num,xnodi[k]) # Il denominatore e il numeratore valutato in xk --> quindi e un numero
     return num/den
 
-def interpolazione(x, f, xx):
-    n = x.size
-    m = xx.size
+def interpolazione(x, f, z):
+    m = x.size
+    n = z.size
     L = np.zeros((m,n))
-    for k in range(n):
-        L[k,:] = np.polyval(polinomioLagrange(x,k), xx)
+    for k in range(m):
+        L[k,:] = np.polyval(polinomioLagrange(x,k), z)
     return np.dot(f,L)
